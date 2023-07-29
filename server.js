2217cc4120328b7fdc8dc2298ebbce866e8cceea
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-07-29 01:04:30 -0400

    add base for ffmpeg compress

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const express = require('express')
+const app = express()
 const config = require('./config.json')
-const app = require('express')()
-const { proxy } = require('./img/proxy')
 const { params } = require('./img/params')
+const { proxy } = require('./img/proxy')
 const { statusPage } = require('./src/statusPage')
+const { videoCompress } = require('./video/videoCompress')
 
 const Port = config.port || 8080
 
@@ -14,6 +16,11 @@ app.get('/img', params, proxy)
 app.get('/', statusPage)
 
 app.get('/favicon.ico', (req, res) => res.status(204).end())
+
+// Usar express.raw() para lidar com o corpo da requisição como raw bytes
+app.use(express.raw({ type: 'video/*', limit: '100mb' }))
+app.post('/compress-video', videoCompress)
+
 app.listen(Port, () =>
   console.log(`Estou online em ${Port}`)
 )
