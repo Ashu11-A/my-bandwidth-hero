8e6d3c80d9ebbb441f069939f5e9184f6ad4f3ef
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-29 18:09:24 -0400

    Update params.js

--- img/params.js
+++ img/params.js
@@ -3,7 +3,7 @@ const DEFAULT_QUALITY = 40
 function params (req, res, next) {
   let url = req.query.url
   if (Array.isArray(url)) url = url.join('&url=')
-  if (!url) return res.send('bandwidth-hero-proxy').status(404).json({ status: 404, message: 'Não foi possiver encontrar a variavel URl', app: 'bandwidth-hero-proxy', version: '1.0.0', maintainer: 'Ashu-11a'})
+  if (!url) return res.send('bandwidth-hero-proxy').status(404).json({ status: 404, message: 'Não foi possiver encontrar a variavel URl', app: 'bandwidth-hero-proxy', version: '1.0.0', maintainer: 'Ashu-11a' })
   url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://')
   req.params.url = url
   req.params.webp = !req.query.jpeg
